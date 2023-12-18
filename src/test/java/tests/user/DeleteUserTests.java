f3ce45d798093a3f4b75b15404e883514eb2dddd
nlkim <nlkim.hse@gmail.com>
2023-12-19 03:45:56 +0900

    DeleteUserTests - refactoring

--- src/test/java/tests/user/DeleteUserTests.java
+++ src/test/java/tests/user/DeleteUserTests.java
@@ -9,19 +9,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static steps.user.UserSteps.*;
 
 public class DeleteUserTests extends AbstractTest {
-    @DisplayName("Delete user")
+    @DisplayName("Check the functionality of deleting user")
     @Link(name = "Specification", url = "https://petstore.swagger.io/#/")
     @Test
-    public void checkDeletingUser() {
+    public void deletingUserTest() {
         String body = """
                 {
-                  "id": 0,
-                  "username": "mark",
-                  "firstName": "string",
-                  "lastName": "string",
-                  "email": "string",
-                  "password": "string",
-                  "phone": "string",
+                  "id": 56,
+                  "username": "mark_123",
+                  "firstName": "mark",
+                  "lastName": "anderson",
+                  "email": "mark123@gmail.com",
+                  "password": "mark123",
+                  "phone": "010-4069-4594",
                   "userStatus": 0
                 }
                 """;
@@ -32,10 +32,46 @@ public class DeleteUserTests extends AbstractTest {
                 .as("Status-code is different from expected")
                 .isEqualTo(200);
 
-        var deletingUser = deleteUser(spec, "mark");
+        var deletingUser = deleteUser(spec, "mark_123");
 
         assertThat(deletingUser.getStatusCode())
                 .as("Status-code is different from expected")
                 .isEqualTo(200);
+    }
+
+    @DisplayName("Check the functionality of deleting user twice - negative case")
+    @Link(name = "Specification", url = "https://petstore.swagger.io/#/")
+    @Test
+    public void deletingUserTwiceTest() {
+        String body = """
+                {
+                  "id": 87,
+                  "username": "jane_123",
+                  "firstName": "jane",
+                  "lastName": "davis",
+                  "email": "jane123@gmail.com",
+                  "password": "jane123",
+                  "phone": "010-8765-4594",
+                  "userStatus": 0
+                }
+                """;
+
+        var creatingUser = createUser(spec, body);
+
+        assertThat(creatingUser.getStatusCode())
+                .as("Status-code is different from expected")
+                .isEqualTo(200);
+
+        var deletingUser = deleteUser(spec, "jane_123");
+
+        assertThat(deletingUser.getStatusCode())
+                .as("Status-code is different from expected")
+                .isEqualTo(200);
+
+        var deletingUserTwice = deleteUser(spec, "jane_123");
+
+        assertThat(deletingUserTwice.getStatusCode())
+                .as("Status-code is different from expected")
+                .isEqualTo(404);
     }
 }
